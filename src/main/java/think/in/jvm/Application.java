368a8d84ce16f32fbcf9a9e393eb8d3d77868f5d
zhang1990zxc <1018736264@qq.com>
2020-12-16 14:14:18 +0800

    add

---
+++ src/main/java/think/in/jvm/Application.java
@@ -0,0 +1,11 @@
+package think.in.jvm;
+
+/**
+ * @ClassName Application
+ * @Description 整条街最靓的仔，写点注释吧
+ * @Author 天涯
+ * @Date 2020/12/16 14:14
+ * @Version 1.0
+ **/
+public class Application {
+}
